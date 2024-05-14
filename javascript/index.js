8d695b54d2f161109772fcaff7c425815d7e9bc1
DHRUV1891 <96827779+DHRUV1891@users.noreply.github.com>
2024-05-15 00:15:27 +0530

    changes to js file

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,33 @@
-getname();
-console.log(x);
+console.log("Thala for a reason");
+var age = 18 ;  // variable can be redeclared and updated 
+var age = 20;
+let nam = "John" ; // variable cannot be redeclared but can be  updated
+nam = "jadeja";
+console.log(nam) ;
+console.log(age) ;
 
-var x = 7 ;
+const name = "Dhoni";  // const declaration must be initialized
+console.log(name) ;
 
-function getname()
-{
-    console.log("Thala for a Reason");
+let team =null;
+console.log(team) ;
+let winner ;
+console.log(winner) ;
+
+let number = BigInt("124215");
+console.log(number) ;
+
+let y = Symbol("hello");
+console.log(y) ;
+
+const student = {
+    fullname : "Dhoni",
+    age :42,
+    team : "csk",
+    trophys:5
 }
+
+console.log(student) ;
+console.log(typeof student) ;
+
+
---
+++ javascript/practise.js
@@ -0,0 +1,7 @@
+const product ={
+    name: pen,
+    rating:4.5,
+    offer:60,
+    price:270,
+    isDeal:true
+}
