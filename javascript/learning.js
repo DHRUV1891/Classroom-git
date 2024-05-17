e65eed6a23bcb20168153025f241caf949fe3df5
DHRUV1891 <96827779+DHRUV1891@users.noreply.github.com>
2024-05-17 20:38:42 +0530

    updated

---
+++ javascript/learning.js
@@ -0,0 +1,29 @@
+//Comments are never executed in JavaScript
+/* THis is a multi line comment */
+
+// Operators in JS
+/*perform operations on Data
+Modulus 
+Exponentiation
+Increment
+Decrement */
+
+let name = "123"
+let sum = name + 4;
+console.log(sum);
+
+let a =11
+let b ="11"
+let c = a%b;
+console.log(c);
+
+console.log(2**3);
+
+if(a==b){
+    console.log(true);
+}
+    else 
+    console.log(false);
+
+
+  
---
+++ javascript/question.js
@@ -0,0 +1,5 @@
+// get a user input number using prompt check for multiple of 5 
+let num =prompt("Enter a number  ");
+if(num%5===0){
+    console.log( num,"is multiple of 5");
+}l
